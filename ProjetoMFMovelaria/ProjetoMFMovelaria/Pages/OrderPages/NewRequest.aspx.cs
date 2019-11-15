f32359477aed16c9c615b0539d76447f2fdb7926
Schuansk Lucas Tenorio Torres <schuansk@hotmail.com.br>
2019-11-15 13:30:22 -0300

    webform add SendEmail ListOrders NewRequest

---
+++ ProjetoMFMovelaria/ProjetoMFMovelaria/Pages/OrderPages/NewRequest.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjetoMFMovelaria.Pages.OrderPages
+{
+    public partial class NewRequest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
